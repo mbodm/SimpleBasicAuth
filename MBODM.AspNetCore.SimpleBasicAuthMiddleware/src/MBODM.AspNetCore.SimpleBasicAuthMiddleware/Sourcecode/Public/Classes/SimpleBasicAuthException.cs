abdcb1dff5e277a90959ef31da75b3b8864648b4
MBODM <webmaster@mbodm.com>
2016-07-01 04:07:33 +0200

    Added folder structure

---
+++ MBODM.AspNetCore.SimpleBasicAuthMiddleware/src/MBODM.AspNetCore.SimpleBasicAuthMiddleware/Sourcecode/Public/Classes/SimpleBasicAuthException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBODM.AspNetCore.SimpleBasicAuthMiddleware
+{
+    public class SimpleBasicAuthException : Exception
+    {
+        public SimpleBasicAuthException() { }
+        public SimpleBasicAuthException(string message) : base(message) { }
+        public SimpleBasicAuthException(string message, Exception inner) : base(message, inner) { }
+    }
+}
